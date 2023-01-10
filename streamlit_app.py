c9f5e99a3b3d69a6af56071c60f1e42d06cdc843
Omar S. Gómez <omargomez2@gmail.com>
2023-01-10 12:34:38 -0500

    Update streamlit_app.py
    bug fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ my_cur = my_cnx.cursor()
 
 streamlit.title("Clothing Catalog")
 
-
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
@@ -31,14 +30,9 @@ option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
 
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
-color_or_style = '" + option + "';")
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
-streamlit.image(
-df2[0],
-width=400,
-caption= product_caption
-)
+streamlit.image(df2[0], width=400, caption= product_caption)
 streamlit.write('Price: ', df2[1])
 streamlit.write('Sizes Available: ',df2[2])
 streamlit.write(df2[3])
