66c4d499450ad3fe0305a2ef40c39f339477c25f
Omar S. Gómez <omargomez2@gmail.com>
2023-01-13 13:36:23 -0500

    Update streamlit_app.py
    caps letters changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_cur = conn.cursor()
 streamlit.title("Clothing Catalog")
 
 # run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from catalog_for_website")
+my_cur.execute("select COLOR_OR_STYLE from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
 # put the dafta into a dataframe
@@ -41,7 +41,7 @@ option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
 # use the option selected to go back and get all the info from the database
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
+my_cur.execute("select DIRECT_URL, PRICE, SIZE_LIST, UPSELL_PRODUCT_DESC from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
 streamlit.image(df2[0], width=400, caption= product_caption)
 streamlit.write('Price: ', df2[1])
